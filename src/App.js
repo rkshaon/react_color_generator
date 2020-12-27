2c8665fb6293d8b315f3bf72be4e65a07256df56
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2020-12-27 17:34:17 +0600

    complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import SingleColor from './SingleColor'
+
+import Values from 'values.js'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [color, setColor] = useState('#f15025');
+  const [error, setError] = useState(false);
+  const [list, setList] = useState(new Values('#f15025').all(10));
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      let colors = new Values(color).all(10);
+      setList(colors);
+      // console.log(colors);
+    } catch (e) {
+      setError(true);
+      console.log(e);
+    }
+  }
+
+  return(
+    <>
+      <section className="container">
+        <h3>color generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={color}
+            placeholder="#f15025"
+            onChange={(e) => {setColor(e.target.value)}}
+            className={`${error ? 'error' : null}`} />
+          <button className="btn" type="submit">Generate</button>
+        </form>
+      </section>
+      <section className="colors">
+        {list.map((color, index) => {
+          console.log(color);
+          return(
+            <SingleColor
+              key={index} {...color}
+              index={index}
+              hexColor={color.hex} />);
+        })}
+      </section>
+    </>
   );
 }
 
-export default App;
+export default App
